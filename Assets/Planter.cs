d4ea383b08934a5ca0cb5a5169211832a87e3d54
Nakata Nobuyuki <nobu.nakata@gmail.com>
2016-02-29 03:59:41 +0900

    no message

---
+++ Assets/Planter.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Planter : MonoBehaviour {
+	public GameObject[] plants;
+	public Collider groundCollider;
+	public float range = 1f;
+
+	float _lastHitTime;
+	Vector3 _lastHitPoint;
+
+	void OnEnable() {
+		_lastHitTime = float.MinValue;
+	}
+	void Update () {
+		if (Input.GetMouseButton (0)) {
+			RaycastHit hit;
+			var ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (groundCollider.Raycast (ray, out hit, float.MaxValue)) {
+				_lastHitTime = Time.timeSinceLevelLoad;
+				_lastHitPoint = hit.point;
+
+					var p = Instantiate (plants [Random.Range (0, plants.Length)]);
+					p.transform.SetParent (transform, false);
+					p.transform.position = hit.point;
+			}
+		}
+	}
+	void OnDrawGizmos() {
+		if (!Application.isPlaying)
+			return; 
+		
+		if ((Time.timeSinceLevelLoad - _lastHitTime) < 3f) {
+			Gizmos.color = Color.red;
+			Gizmos.DrawWireSphere (_lastHitPoint, range);
+		}
+	}
+}
---
+++ Assets/Plants/ScreenSpacePlane.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(MeshFilter))]
+public class ScreenSpacePlane : MonoBehaviour {
+	public Camera targetCamera;
+	public float distance = 100f;
+
+	Mesh _localMesh;
+	Vector3[] _vertices;
+
+	void OnEnable() {
+		_vertices = new Vector3[] {
+			new Vector3 (-1f, -1f, 0f),
+			new Vector3 (1f, -1f, 0f),
+			new Vector3 (-1f, 1f, 0f),
+			new Vector3 (1f, 1f, 0f)
+		};
+
+		_localMesh = new Mesh ();
+		_localMesh.MarkDynamic ();
+		_localMesh.vertices = _vertices;
+		_localMesh.uv = new Vector2[]{ 
+			new Vector2 (0f, 0f), new Vector2 (1f, 0f), new Vector2 (0f, 1f), new Vector2 (1f, 1f) };
+		_localMesh.triangles = new int[]{ 0, 3, 1, 0, 2, 3 };
+		_localMesh.RecalculateBounds ();
+		_localMesh.RecalculateNormals ();
+
+		var mf = GetComponent<MeshFilter> ();
+		if (mf != null)
+			mf.sharedMesh = _localMesh;
+
+		var mc = GetComponent<MeshCollider> ();
+		if (mc != null)
+			mc.sharedMesh = _localMesh;
+	}
+	void Update () {
+		var cameraSize = targetCamera.orthographicSize;
+		transform.SetParent (targetCamera.transform, false);
+		transform.localPosition = Vector3.forward * distance;
+		transform.localRotation = Quaternion.identity;
+		transform.localScale = new Vector3 (targetCamera.aspect * cameraSize, cameraSize, 1f);
+	}
+}
