dbc8507f5ad2439702144dbbefb9eb15c753e880
unknown <nnakata@LAB-N991.team-lab.local>
2016-03-28 15:09:52 +0900

    no message

--- Assets/Packages/Garden/Examples/GLFigureTest.cs
+++ Assets/Packages/Garden/Examples/GLFigureTest.cs
@@ -5,10 +5,7 @@ using Gist;
 namespace Garden {
 
 	public class GLFigureTest : MonoBehaviour {
-		public Transform target;
-		public Vector2 size;
-		public Color bodyColor = Color.green;
-		public Color lineColor = Color.white;
+        public Data[] datas;
 
 		GLFigure _figure;
 
@@ -19,8 +16,47 @@ namespace Garden {
 			_figure.Dispose ();
 		}
 		void OnRenderObject() {
-			_figure.FillCircle (target.position, Camera.main.transform.rotation, size, bodyColor);
-			_figure.DrawCircle (target.position, Camera.main.transform.rotation, size, lineColor);
+            foreach (var d in datas) {
+                d.Draw (_figure);
+            }
 		}
+
+        [System.Serializable]
+        public class Data {
+            [System.Flags]
+            public enum RenderTypeEnum { None = 0, Line = (1 << 0), Fill = (1 << 1), Whole = Line | Fill }
+            public enum ZModeEnum { LEqual = 0, Overlay }
+
+            public RenderTypeEnum renderType;
+            public ZModeEnum ztest;
+            public Transform target;
+            public Vector2 size;
+            public Color bodyColor;
+            public Color lineColor;
+
+            public void ApplyZMode(GLFigure f) {
+                switch (ztest) {
+                case ZModeEnum.LEqual:
+                    f.ZTestMode = GLFigure.ZTestEnum.LESSEQUAL;
+                    f.ZWriteMode = true;
+                    break;
+                case ZModeEnum.Overlay:
+                    f.ZTestMode = GLFigure.ZTestEnum.ALWAYS;
+                    f.ZWriteMode = false;
+                    break;
+                }
+            }
+            public void Draw(GLFigure f) {
+                ApplyZMode (f);
+
+                if ((renderType & Data.RenderTypeEnum.Fill) != 0) {
+                    f.FillCircle (target.position, Camera.main.transform.rotation, size, bodyColor);
+                }
+                if ((renderType & Data.RenderTypeEnum.Line) != 0) {
+                    f.DrawCircle (target.position, Camera.main.transform.rotation, size, lineColor);
+                }                
+            }
+
+        }
 	}
 }
--- Assets/Packages/Gist/GLFigure.cs
+++ Assets/Packages/Gist/GLFigure.cs
@@ -3,9 +3,20 @@ using System.Collections;
 
 namespace Gist {
 	public class GLFigure : System.IDisposable {
+        public const string PROP_COLOR = "_Color";
+        public const string PROP_SRC_BLEND = "_SrcBlend";
+        public const string PROP_DST_BLEND = "_DstBlend";
+        public const string PROP_ZWRITE = "_ZWrite";
+        public const string PROP_ZTEST = "_ZTest";
+        public const string PROP_CULL = "_Cull";
+        public const string PROP_ZBIAS = "_ZBias";
+
 		public const float TWO_PI_RAD = 2f * Mathf.PI;
 		public const int SEGMENTS = 36;
 		public const string LINE_SHADER = "Hidden/Internal-Colored";
+        
+        public enum ZTestEnum { NEVER = 1, LESS = 2, EQUAL = 3, LESSEQUAL = 4,
+            GREATER = 5, NOTEQUAL = 6, GREATEREQUAL = 7, ALWAYS = 8 };
 
 		Material _lineMat;
 
@@ -15,6 +26,15 @@ namespace Gist {
 				Debug.LogErrorFormat ("Line Shader not found : {0}", LINE_SHADER);
 			_lineMat = new Material (lineShader);
 		}
+
+        public bool ZWriteMode {
+            get { return _lineMat.GetInt (PROP_ZWRITE) == 1; }
+            set { _lineMat.SetInt (PROP_ZWRITE, value ? 1 : 0); }
+        }
+        public ZTestEnum ZTestMode {
+            get { return (ZTestEnum)_lineMat.GetInt (PROP_ZTEST); }
+            set { _lineMat.SetInt (PROP_ZTEST, (int)value); }
+        }
 
 		public void DrawCircle(Vector3 center, Quaternion look, Vector2 size, Color color) {
 			var scale = new Vector3 (size.x, size.y, 1f);
